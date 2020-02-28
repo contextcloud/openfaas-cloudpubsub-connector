c30ab0849ed7e5e3f07ecdb8ec4c8baf98815afa
Chris Kolenko <chris@webcanvas.com.au>
2020-02-28 17:33:17 +1100

    Get or create subscriptions

--- pubsub/broker.go
+++ pubsub/broker.go
@@ -11,15 +11,11 @@ import (
 	"github.com/openfaas-incubator/connector-sdk/types"
 )
 
-const subID = "openfaas_pubsub_worker_group"
+const subID = "openfaas_connector_"
 
 // Broker used to subscribe to NATS subjects
 type Broker interface {
 	Subscribe(types.Controller, []string) error
-}
-
-type broker struct {
-	client *pubsub.Client
 }
 
 // NewBroker loops until we are able to connect to the NATS server
@@ -27,10 +23,7 @@ func NewBroker(config BrokerConfig) (Broker, error) {
 	broker := &broker{}
 
 	for {
-		ctx := context.Background()
-		ctx, _ = context.WithTimeout(ctx, config.ConnTimeout)
-
-		client, err := pubsub.NewClient(ctx, config.ProjectID)
+		client, err := pubsub.NewClient(context.Background(), config.ProjectID)
 		if client != nil && err == nil {
 			broker.client = client
 			break
@@ -48,6 +41,41 @@ func NewBroker(config BrokerConfig) (Broker, error) {
 	return broker, nil
 }
 
+type broker struct {
+	client *pubsub.Client
+}
+
+func (b *broker) Topic(ctx context.Context, topic string) (*pubsub.Topic, error) {
+	t := b.client.Topic(topic)
+	exists, err := t.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Get Topic: %w", err)
+	}
+	if !exists {
+		return nil, fmt.Errorf("Topic doesn't exist")
+	}
+	return t, nil
+}
+
+func (b *broker) Subscription(ctx context.Context, id string, topic *pubsub.Topic) (*pubsub.Subscription, error) {
+	sub := b.client.Subscription(id)
+	exists, err := sub.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Get Subscription: %w", err)
+	}
+	if exists {
+		return sub, nil
+	}
+
+	return b.client.CreateSubscription(ctx, id, pubsub.SubscriptionConfig{
+		Topic:       topic,
+		AckDeadline: 20 * time.Second,
+	})
+}
+
+func (b *broker) Receive(ctx context.Context, sub *pubsub.Subscription, controller types.Controller) {
+}
+
 // Subscribe to a list of NATS subjects and block until interrupted
 func (b *broker) Subscribe(controller types.Controller, topics []string) error {
 	log.Printf("Configured topics: %v", topics)
@@ -60,34 +88,34 @@ func (b *broker) Subscribe(controller types.Controller, topics []string) error {
 	wg.Add(1)
 
 	ctx := context.Background()
+	cctx, cancel := context.WithCancel(ctx)
+	defer func() {
+		cancel()
+		b.client.Close()
+	}()
 
 	subs := []*pubsub.Subscription{}
 	for _, topic := range topics {
 		log.Printf("Binding to topic: %q", topic)
 
-		t := b.client.Topic(topic)
-		ok, err := t.Exists(ctx)
+		t, err := b.Topic(ctx, topic)
 		if err != nil {
 			return fmt.Errorf("Get Topic: %w", err)
 		}
-		if !ok {
-			return fmt.Errorf("Topic doesn't exist")
-		}
 
-		sub, err := b.client.CreateSubscription(ctx, subID, pubsub.SubscriptionConfig{
-			Topic:       t,
-			AckDeadline: 20 * time.Second,
-		})
+		sub, err := b.Subscription(ctx, topic, t)
 		if err != nil {
-			return fmt.Errorf("CreateSubscription: %w", err)
+			return fmt.Errorf("Get Subscription: %w", err)
 		}
 
-		if err := sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-			controller.InvokeWithContext(ctx, topic, &m.Data)
-			m.Ack()
-		}); err != nil {
-			return fmt.Errorf("Receive: %w", err)
-		}
+		go func() {
+			if err := sub.Receive(cctx, func(ctx context.Context, m *pubsub.Message) {
+				controller.InvokeWithContext(ctx, topic, &m.Data)
+				m.Ack()
+			}); err != nil {
+				panic(fmt.Sprintf("Could not run receive %v", err))
+			}
+		}()
 
 		subs = append(subs, sub)
 	}
@@ -97,6 +125,5 @@ func (b *broker) Subscribe(controller types.Controller, topics []string) error {
 	}
 
 	wg.Wait()
-	b.client.Close()
 	return nil
 }
